7ab702ecba34405b8c83112aa6aee2ad6e7aa3b8
Martin Jencka <futilrevenge@gmail.com>
2021-10-08 21:56:08 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Esoterra RPG for Foundry VTT
+#### This is an unofficial version of the Esoterra RPG system, which is the property of Isaac Williams & Losing Games.
+Esoterra can be purchased here: https://esoterra.com/
+```
+- The official game Weapon, Item, and Spell Icons by Isaac Williams are licensed under CC-BY.
+- The sample Rat portrait from the official book was allowed to be used with permission from Isaac Williams
+```
+# Features:
+## Styled character sheets
+Character, Creature, and NPC sheets were built to resemble the standard print sheets.
+
+
+## Draggable & Transferrable Item Cards
+Inventory is handled through item cards, which can be moved around freely, moved between sheets, or copied with CTRL+Drag.
+
+Cards are styled to resemble the official game cards, with automated rolls, usage dots, and more being included.
+
+For usage, left click the dots to increase, and right click to decrease. You can also swap medium weapon damage between one handed and two by clicking the damage value.
+
+## Item Compendiums
+Every Item, Weapon, Condition, and Spell from the game has been included inside of the system's compendiums.
+
+## Storage Sheets with Adjustable Dimensions
+Have a cart, chest, bank, etc that players want to store items in? Or maybe you have a treasure horde that you want to allow players to peruse through. You can create 'Storage' actors with adjustable slot counts.
+
+## Automated Skill Rolls
+You can roll your stats using the built in advantage/disadvantage system
+
+## Macro Support for Weapons, Items, and Skills
+For items, you can simply drag them into the hotbar, or use this script:
+
+```
+game.esoterra.rollItemMacro("itemName");
+```
+For Skills, create a new macro with this
+```
+game.esoterra.rollStatMacro();
+```
+
+# Screenshots
+![Alt text](https://i.imgur.com/4PYBj8X.jpg "Game Example")
+
+_Esoterra ©2020 Isaac Williams & Losing Games_
---
+++ module/esoterra.js
@@ -0,0 +1,210 @@
+// Import Modules
+import { EsoterraActor } from "./actor/actor.js";
+import { EsoterraActorSheet } from "./actor/actor-sheet.js";
+import { EsoterraHirelingSheet } from "./actor/hireling-sheet.js";
+import { EsoterraCreatureSheet } from "./actor/creature-sheet.js";
+import { EsoterraStorageSheet } from "./actor/storage-sheet.js";
+
+import { EsoterraItem } from "./item/item.js";
+import { EsoterraItemSheet } from "./item/item-sheet.js";
+
+import {
+  registerSettings
+} from "./settings.js";
+
+Hooks.once('init', async function () {
+
+  game.esoterra = {
+    EsoterraActor,
+    EsoterraItem,
+    rollItemMacro,
+    rollStatMacro
+  };
+
+  registerSettings();
+
+
+  /**
+   * Set an initiative formula for the system
+   * @type {String}
+   */
+  CONFIG.Combat.initiative = {
+    formula: "1d100",
+    decimals: 2
+  };
+
+  // Define custom Entity classes
+  CONFIG.Actor.documentClass = EsoterraActor;
+  CONFIG.Item.documentClass = EsoterraItem;
+
+
+  // Register sheet application classes
+  Actors.unregisterSheet("core", ActorSheet);
+
+  Actors.registerSheet("esoterra", EsoterraActorSheet, {
+    types: ['character'],
+    makeDefault: true
+  });
+  Actors.registerSheet("esoterra", EsoterraHirelingSheet, {
+    types: ['hireling'],
+    makeDefault: false
+  });
+  Actors.registerSheet("esoterra", EsoterraCreatureSheet, {
+    types: ['creature'],
+    makeDefault: false
+  });
+  Actors.registerSheet("esoterra", EsoterraStorageSheet, {
+    types: ['storage'],
+    makeDefault: false
+  });
+
+  Items.unregisterSheet("core", ItemSheet);
+  Items.registerSheet("esoterra", EsoterraItemSheet, { makeDefault: true });
+
+  // If you need to add Handlebars helpers, here are a few useful examples:
+  Handlebars.registerHelper('concat', function () {
+    var outStr = '';
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != 'object') {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+
+  Handlebars.registerHelper('toLowerCase', function (str) {
+    return str.toLowerCase();
+  });
+
+  CONFIG.Combat.initiative = {
+    formula: "-1d20+@stats.dexterity.value",
+    decimals: 2
+  };
+
+  // preloadHandlebarsTemplates();
+});
+
+/**
+ * Set default values for new actors' tokens
+ */
+Hooks.on("preCreateActor", (createData) => {
+  let disposition = CONST.TOKEN_DISPOSITIONS.NEUTRAL;
+
+  if (createData.type == "creature") {
+    disposition = CONST.TOKEN_DISPOSITIONS.HOSTILE
+  }
+
+  // Set wounds, advantage, and display name visibility
+  mergeObject(createData,
+    {
+      "token.bar1": { "attribute": "health" },        // Default Bar 1 to Health 
+      "token.bar2": { "stat": "strength" },      // Default Bar 2 to Insanity
+      "token.displayName": CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER,     // Default display name to be on owner hover
+      "token.displayBars": CONST.TOKEN_DISPLAY_MODES.OWNER_HOVER,     // Default display bars to be on owner hover
+      "token.disposition": disposition,                               // Default disposition to neutral
+      "token.name": createData.name                                   // Set token name to actor name
+    })
+
+
+  if (createData.type == "character") {
+    createData.token.vision = true;
+    createData.token.actorLink = true;
+  }
+})
+
+// async function preloadHandlebarsTemplates() {
+//   const templatePaths = [
+//       "systems/esoterra/templates/item/item-card.html"
+//   ];
+//   return loadTemplates(templatePaths);
+// }
+
+
+Hooks.once("ready", async function () {
+  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
+  Hooks.on("hotbarDrop", (bar, data, slot) => createEsoterraMacro(data, slot));
+});
+
+
+
+/* -------------------------------------------- */
+/*  Hotbar Macros                               */
+/* -------------------------------------------- */
+
+/**
+ * Create a Macro from an Item drop.
+ * Get an existing item macro if one exists, otherwise create a new one.
+ * @param {Object} data     The dropped data
+ * @param {number} slot     The hotbar slot to use
+ * @returns {Promise}
+ */
+async function createEsoterraMacro(data, slot) {
+  if (data.type !== "Item") return;
+  if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
+  const item = data.data;
+
+  // Create the macro command
+  let command = `game.esoterra.rollItemMacro("${item.name}");`;
+
+
+  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
+  if (!macro) {
+    macro = await Macro.create({
+      name: item.name,
+      type: "script",
+      img: item.img,
+      command: command,
+      flags: {
+        "esoterra.itemMacro": true
+      }
+    });
+  }
+  game.user.assignHotbarMacro(macro, slot);
+  return false;
+}
+
+
+/**
+ * Roll Macro from a Weapon.
+ * @param {string} itemName
+ * @return {Promise}
+ */
+function rollItemMacro(itemName) {
+  const speaker = ChatMessage.getSpeaker();
+  let actor;
+  if (speaker.token) actor = game.actors.tokens[speaker.token];
+  if (!actor) actor = game.actors.get(speaker.actor);
+  const item = actor ? actor.items.find(i => i.name === itemName) : null;
+  if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
+
+  return actor.rollItem(item.id);
+}
+
+
+/**
+ * Roll Stat.
+ * @param {string} statName
+ * @return {Promise}
+ */
+function rollStatMacro() {
+  var selected = canvas.tokens.controlled;
+  const speaker = ChatMessage.getSpeaker();
+
+  if (selected.length == 0) {
+    selected = game.actors.tokens[speaker.token];
+  }
+
+  let actor;
+  if (speaker.token) actor = game.actors.tokens[speaker.token];
+  if (!actor) actor = game.actors.get(speaker.actor);
+  const stat = actor ? Object.entries(actor.data.data.stats) : null;
+
+
+  // if (stat == null) {
+  //   ui.notifications.info("Stat not found on token");
+  //   return;
+  // }
+
+
+  return actor.rollStatSelect(stat);
+}
